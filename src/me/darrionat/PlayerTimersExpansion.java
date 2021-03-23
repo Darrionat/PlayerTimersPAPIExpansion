a8a4ac6be8cae38ef713697a01eea72013a9f4d0
Darrion <52856180+Darrionat@users.noreply.github.com>
2021-03-23 17:13:44 -0400

    Initial Commit

---
+++ src/me/darrionat/PlayerTimersExpansion.java
@@ -0,0 +1,128 @@
+package me.darrionat;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import me.darrionat.playertimers.PlayerTimer;
+import me.darrionat.playertimers.PlayerTimersPlugin;
+
+public class PlayerTimersExpansion extends PlaceholderExpansion {
+
+	private PlayerTimersPlugin plugin;
+
+	@Override
+	public String getIdentifier() {
+		return "playerTimers";
+	}
+
+	@Override
+	public String getAuthor() {
+		return "Darrionat";
+	}
+
+	@Override
+	public String getVersion() {
+		return "1.0.0";
+	}
+
+	/**
+	 * Returns the name of the required plugin.
+	 *
+	 * @return {@code DeluxeTags} as String
+	 */
+	@Override
+	public String getRequiredPlugin() {
+		return "PlayerTimers";
+	}
+
+	/**
+	 * Used to check if the expansion is able to register.
+	 * 
+	 * @return true or false depending on if the required plugin is installed
+	 */
+	@Override
+	public boolean canRegister() {
+		if (!Bukkit.getPluginManager().isPluginEnabled(getRequiredPlugin())) {
+			return false;
+		}
+		plugin = (PlayerTimersPlugin) Bukkit.getPluginManager().getPlugin(getRequiredPlugin());
+		return plugin != null;
+	}
+
+	/**
+	 * This method is called when a placeholder is used and matches the set
+	 * {@link #getIdentifier() identifier}
+	 *
+	 * @param offlinePlayer The player to parse placeholders for
+	 * @param params        The part after the identifier
+	 *                      ({@code %identifier_params%})
+	 *
+	 * @return returns value or possibly {@code null}
+	 */
+	@Override
+	public String onRequest(OfflinePlayer offlinePlayer, String params) {
+		int n;
+		try {
+			n = Integer.parseInt(params);
+		} catch (NumberFormatException exe) {
+			return "Invalid Placeholder";
+		}
+
+		List<PlayerTimer> sortedTimes = getSortedExistingTimes();
+		if (sortedTimes.size() >= n && n > 0) {
+			PlayerTimer time = sortedTimes.get(n - 1);
+			OfflinePlayer player = Bukkit.getOfflinePlayer(time.getPlayer());
+
+			String durationStr = getDurationString(time.getDuration());
+
+			return String.valueOf(n) + " " + player.getName() + ": " + durationStr;
+		}
+
+		return null;
+	}
+
+	/**
+	 * Gets and sorts the existing times from shortest duration to most
+	 * 
+	 * @return returns a list of {@code PlayerTimer} sorted from shortest duration
+	 *         to longest
+	 */
+	public List<PlayerTimer> getSortedExistingTimes() {
+		List<PlayerTimer> list = plugin.getTimesRepository().getAllSavedTimes();
+
+		// Sort the list
+		Collections.sort(list, new Comparator<PlayerTimer>() {
+			public int compare(PlayerTimer t1, PlayerTimer t2) {
+				Long duration = t1.getDuration();
+				return duration.compareTo(t2.getDuration());
+			}
+		});
+		return list;
+	}
+
+	public static String getDurationString(long millis) {
+		long totalSeconds = millis / 1000;
+		double days = Math.floor(totalSeconds / 86400);
+		double hours = Math.floor(totalSeconds % 86400 / 3600);
+		double minutes = Math.floor(totalSeconds % 3600 / 60);
+		double seconds = Math.floor(totalSeconds % 60);
+
+		String duration = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+
+		if (days < 1) {
+			duration = duration.replace(days + "d ", "");
+			if (hours < 1) {
+				duration = duration.replace(hours + "h ", "");
+				if (minutes < 1) {
+					duration = duration.replace(minutes + "m ", "");
+				}
+			}
+		}
+		return duration;
+	}
+}
